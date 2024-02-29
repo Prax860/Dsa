e2ef5623a93c1c6b448422d3d4a065e51fdf0fc9
Prax <132813089+Prax860@users.noreply.github.com>
2024-02-29 11:47:34 +0530

    Create deletion_at_endinll.c

---
+++ deletion_at_endinll.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *link;
+};
+
+struct node* del(struct node* head){
+    if(head == NULL){
+        printf("The list is already empty\n");
+        return NULL;
+    }
+    else{
+        struct node* temp = head;
+        head = head->link;
+        free(temp);
+        temp = NULL;
+    }
+    return head;
+}
+
+int main(){
+    struct node* head = NULL;
+    struct node* current;
+    int n ;
+    scanf("%d",&n);
+    int data;
+    char ch;
+    printf("Do you want to enter a node (y/n)? ");
+    scanf(" %c", &ch);
+    for(int i=0;i<=n;i++){
+        current = (struct node*) malloc(sizeof(struct node));
+        printf("Enter the data: ");
+        scanf("%d", &data);
+        current->data = data;
+        current->link = head;
+        head = current;
+    }
+    head = del(head);
+    struct node* ptr = head;
+    
+    while(ptr!=NULL){
+        printf("%d",ptr->data);
+        ptr = ptr->link;
+    }
+    return 0;
+}
